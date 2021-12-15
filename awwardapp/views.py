d4d1e7bc1d5b0d879d633bb3fb2064ac2813760d
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-15 10:48:22 +0300

    created view for the project, what is required

--- awwardapp/views.py
+++ awwardapp/views.py
@@ -1,3 +1,87 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from .models import Profile, Project
+from django.contrib.auth.models import User
+from awwardapp.forms import UpdateProfileForm, SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
+
+
 
 # Create your views here.
+def index(request):
+    profile = Profile.objects.all()
+    projects = Project.objects.all()
+    return render(request, 'index.html', {"profile": profile, "projects": projects})
+
+@login_required(login_url='/accounts/login/')
+def search(request):
+    if 'project' in request.GET and request.GET['project']:
+        project = request.GET.get("project")
+        results = Project.search_project(project)
+        message = f'project'
+        return render(request, 'search.html', {'projects': results, 'message': message})
+    else:
+        message = "You haven't searched for anything, please try again"
+    return render(request, 'search.html', {'message': message})
+
+def signup(request):
+    print('here')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db() 
+            user.profile.birth_date = form.cleaned_data.get('full_name')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Project.objects.filter(user=current_user.id).all
+    return render(request, 'registration/profile.html', {"projects": projects})
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request, id):
+    profile_object = get_object_or_404(Profile, user_id=id)
+    user_object = get_object_or_404(User, id=id)
+    profile_form = UpdateProfileForm(request.POST or None, request.FILES, instance=profile_object)
+    user_form = UpdateUserForm(request.POST or None, instance=user_object)
+    if profile_form.is_valid() and user_form.is_valid():
+        profile_form.save()
+        user_form.save()
+        return HttpResponseRedirect("/profile")
+
+    return render(request, "registration/update_profile.html", {"form": profile_form, "form2": user_form})
+
+@login_required(login_url='/accounts/login')
+def post_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            post_project = form.save(commit=False)
+            post_project.user = current_user
+            post_project.save()
+            return redirect('index')
+    else:
+        form = ProjectForm()
+    return render(request, 'projects.html', {"form": form})
+
+@login_required(login_url='/accounts/login')
+def project(request, id):
+    project = Project.objects.get(id=id)
+    reviews = Rates.objects.all()
+    return render(request, 'view-project.html', {"project": project, "reviews": reviews})
+
