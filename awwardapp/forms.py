79405dd61df0b39c6c4b37f55a9068e211a727ca
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-15 10:47:38 +0300

    this help to define user form for the application

---
+++ awwardapp/forms.py
@@ -0,0 +1,42 @@
+
+from .models import Profile,Project
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class showprojectform(ModelForm):
+    class Meta:
+        model=Project
+        fields=('image',
+                'title',
+                'description',
+                'category',
+                'location',
+                 'url',
+               )
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('profile_pic','bio','contact') 
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user'] 
