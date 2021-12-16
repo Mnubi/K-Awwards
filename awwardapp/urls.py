e988760b1dde641eebabbcd8015135575b55e63a
mnubi <rodger.kamau@student.moringaschool.com>
2021-12-16 20:07:27 +0300

    added path to search and posting projects

--- awwardapp/urls.py
+++ awwardapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns=[
     path('',views.index,name = 'index'),
     path('profile/', views.profile, name='profile'),
     path('post/profile/', views.profile,name='profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
+    path('post/',views.post_project,name='post_project'),
+    path('search/', views.search, name='search'),
 
 ]
